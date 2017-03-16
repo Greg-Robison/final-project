b6a45456fcbf26751e660412723c37c8b73320c3
Greg Robison <gjrobison64@gmail.com>
2017-03-16 17:09:29 -0400

    publish: fixed minor bug
    generated from commit a528bf60b9767b018c61ec37b03454ad8589117b

--- js/bundle.js
+++ js/bundle.js
@@ -706,7 +706,7 @@ var Backbone = require('backbone');
 
 var Wunderground = Backbone.Model.extend({
 urlRoot: function(){
-  return "http://api.wunderground.com/api/7f941aa7f5bbf3a0/conditions/q/SC/Greenville.json";
+  return "https://api.wunderground.com/api/7f941aa7f5bbf3a0/conditions/q/SC/Greenville.json";
 
 },
 sync: function(method, model, options){
